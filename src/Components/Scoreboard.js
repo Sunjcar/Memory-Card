32f937361355b6872d2606d4db42778773340c62
Raymond Chen <raychen358@gmail.com>
2022-09-21 22:00:27 -0700

    Styling for scoreboard

--- src/Components/Scoreboard.js
+++ src/Components/Scoreboard.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import styledComponents from 'styled-components'
 
 const Scoreboard = ({ currentScore, highScore }) => {
     return (
-       <div className='game-score'>
-           <div>
-           CurrentScore: {currentScore}
-           </div>
-           HighScore: {highScore}
-       </div>
+        <div className='game-score'>
+            <CurrentScore>
+                CurrentScore: {currentScore}
+            </CurrentScore>
+            <HighScore>
+                HighScore: {highScore}
+            </HighScore>
+        </div>
 
     )
 }
 
 export default Scoreboard
+
+const CurrentScore = styledComponents.div`
+padding:.5rem;
+border-radius:10px;
+background-color:green;
+`
+const HighScore =styledComponents.div`
+padding:.5rem;
+border-radius:10px;
+background-color: #fde047`
